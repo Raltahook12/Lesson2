a1149542258adfcad34c81ba43008a48f8f2fa50
korne <korne@DESKTOP-39PUEQ7>
2022-10-29 19:05:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите ЗП за кварталы");
+            
+            int chapt1 = int.Parse(Console.ReadLine());
+            int chapt2 = int.Parse(Console.ReadLine());
+            int chapt3 = int.Parse(Console.ReadLine());
+            
+            int chapt4 = int.Parse(Console.ReadLine());
+            if (age >= 18)
+                Console.WriteLine($"Имя сотрудника: {name} \n Фамилия сотрудника: {lastName} " +
+                    $"\n  Возраст сотрудника: {age}\n Средняя зарплата за год: {(chapt1 + chapt2 + chapt3 + chapt4) / 4}");
+
+            else if (age < 18)
+                Console.WriteLine("Детский труд запрещён");
+            else if (age < 0 | chapt1 < 0 | chapt2 < 0 | chapt3 < 0 | chapt4 < 0)
+                Console.WriteLine("Некорректные данные");
+        }
+
+
+    }
+}
