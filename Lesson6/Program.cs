f2538bc3eed6022292b6926b7a569dd71bc41ab6
Daniil Korneev <korneev06102002@gmai.com>
2022-12-03 18:49:54 +0300

    Учёл изсенения

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -4,24 +4,32 @@ namespace Lesson6
 {
     internal class Program
     {
-        //Тут должен быть мэин
-        Scientist Daniil = new Scientist("Daniil", 20, -10000, "Даже не балаклавр");
+        static void Main(string[] args)
+        {
+            Scientist Daniil = new Scientist("Daniil", 20, -10000, "Даже не балаклавр");
+            Daniil.WriteCharacterisitics();
+            Console.ReadKey();
+        }
     }
     public abstract class Person
     {
         private string _name;
 
-        public string Name { get { return _name; } set { _name = value; } }
+        public string Name { get { return _name; } }
 
         private int _age;
 
-        public int Age { get { return _age; } set { _age = value; } }
+        public int Age { get { return _age; } }
 
         public int Salary;
 
-        public virtual void WriteCharacterisitics()
+        public abstract void WriteCharacterisitics();
+
+        public Person(string name, int age , int salary)
         {
-            ;
+            _age = age;
+            _name = name;
+            Salary = salary;
         }
     }
 
@@ -33,13 +41,10 @@ namespace Lesson6
 
         public override void WriteCharacterisitics()
         {
-            Console.WriteLine($"Работник: {Name} \n Возраст:{Age} \n Зарплата:{salary} \n Учёное звание: {_grade}");
+            Console.WriteLine($"Работник: {Name} \n Возраст:{Age} \n Зарплата:{Salary} \n Учёное звание: {_grade}");
         }
-        public Scientist(string name, int age, int salary, string grade)
+        public Scientist(string name, int age, int salary, string grade) : base(name, age , salary)
         {
-            Name = name;
-            Age = age;
-            Salary = salary;
             Grade = grade;
         }
     }
